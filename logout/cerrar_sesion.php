0d3cee85e7021b76c307cac405b4219d2c8d9710
danielgarcia0612 <daniel.garcia1@serbatic.es>
2025-05-06 13:53:11 +0200

    cambiar la redireccion de cerrar sesion y vaya al inicio en vez de al login

--- logout/cerrar_sesion.php
+++ logout/cerrar_sesion.php
@@ -2,6 +2,6 @@
 session_start();
 session_unset();     
 session_destroy();    //destruinmos sesion y variables
-header("Location: ../login/login.html"); // volvemos la login
+header("Location: ../inicio/index.php"); // volvemos el inicio
 exit();
 ?>
